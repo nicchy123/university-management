16155e07309f4ca2f246d2423175853a408cda00
nicchy123 <nasirchy252@gmail.com>
2023-10-16 18:15:28 +0600

    faculty added

--- src/app/modules/faculty/faculty.validation.ts
+++ src/app/modules/faculty/faculty.validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const updateAdmin = z.object({
+const updateFacultyZodSchema = z.object({
   body: z.object({
     name: z.object({
       firstName: z.string().optional(),
@@ -8,29 +8,18 @@ const updateAdmin = z.object({
       middleName: z.string().optional(),
     }),
     dateOfBirth: z.string().optional(),
-
     gender: z.string().optional(),
-
     bloodGroup: z.string().optional(),
-
     email: z.string().email().optional(),
-
     contactNo: z.string().optional(),
-
     emergencyContactNo: z.string().optional(),
-
     presentAddress: z.string().optional(),
-
     permanentAddress: z.string().optional(),
-
     department: z.string().optional(),
-
     designation: z.string().optional(),
-
-    profileImage: z.string().optional(),
   }),
 });
 
-export const AdminValidation = {
-  updateAdmin,
+export const FacultyValidation = {
+  updateFacultyZodSchema,
 };
